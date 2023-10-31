c8950eef76e31462300f1c7d0ac9109cd0399322
min-jp <mincjiip@gmail.com>
2023-11-01 00:09:40 +0900

    feat: updated the QuestionController

--- src/main/java/teamummmm/musiq/controller/QuestionController.java
+++ src/main/java/teamummmm/musiq/controller/QuestionController.java
@@ -6,20 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import teamummmm.musiq.dto.RequestQuestionDTO;
 import teamummmm.musiq.dto.ResponseDTO;
-import teamummmm.musiq.dto.UserQuestionDTO;
-import teamummmm.musiq.model.UserQuestionEntity;
 import teamummmm.musiq.service.QuestionService;
-import teamummmm.musiq.service.testService;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("question")
 public class QuestionController {
     @Autowired
-    private testService service;  //test
+    private QuestionService service;  // service
 
     @GetMapping("/main")
     public ResponseEntity<?> mainQuestionRequest(
@@ -28,16 +25,17 @@ public class QuestionController {
     ) {
         try {
             // TODO
-            //  dtos에 테스트 서비스 넣음
-            List<UserQuestionEntity> entities = service.test();  // test
-            List<UserQuestionDTO> dtos = entities.stream().map(UserQuestionDTO::new).collect(Collectors.toList());  // UserQuestionDTO 변환
-            ResponseDTO<UserQuestionDTO> response = ResponseDTO.<UserQuestionDTO>builder().data(dtos).build();  // UserQuestionDTO 이용하여 ResponseDTO 초기화
+            //  dtos에 서비스 넣음
+            List<RequestQuestionDTO> dtos = null;  // UserQuestionDTO 생성
+            ResponseDTO<RequestQuestionDTO> response = ResponseDTO.<RequestQuestionDTO>builder()
+                    .data(dtos)
+                    .build();  // UserQuestionDTO 이용하여 ResponseDTO 초기화
             return ResponseEntity.ok().body(response);  // ResponseDTO 리턴
         } catch (Exception e) {
             String error = e.getMessage();
-            // TODO
-            //  String 자료형 수정
-            ResponseDTO<UserQuestionDTO> response = ResponseDTO.<UserQuestionDTO>builder().error(error).build();
+            ResponseDTO<RequestQuestionDTO> response = ResponseDTO.<RequestQuestionDTO>builder()
+                    .error(error)
+                    .build();
             return ResponseEntity.badRequest().body(response);
         }
     }
