ed98c20afd452ef0cd2238ca4b93318e88320714
min-jp <mincjiip@gmail.com>
2023-11-01 04:22:59 +0900

    fix: Changed service code from Field Injection to Constructor Injection

--- src/main/java/teamummmm/musiq/service/PlaySongService.java
+++ src/main/java/teamummmm/musiq/service/PlaySongService.java
@@ -1,7 +1,9 @@
 package teamummmm.musiq.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PlaySongService {
 }
