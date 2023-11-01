b17912834a4729f5c01bf4afb3a22d3bdf11f222
min-jp <mincjiip@gmail.com>
2023-11-01 20:15:35 +0900

    temp: UserQuestionRepository temp code

--- src/main/java/teamummmm/musiq/repository/UserQuestionRepository.java
+++ src/main/java/teamummmm/musiq/repository/UserQuestionRepository.java
@@ -2,8 +2,12 @@ package teamummmm.musiq.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import teamummmm.musiq.model.UserProfileEntity;
 import teamummmm.musiq.model.UserQuestionEntity;
+
+import java.util.List;
 
 @Repository
 public interface UserQuestionRepository extends JpaRepository<UserQuestionEntity, Long> {
+    List<UserQuestionEntity> findByUser(UserProfileEntity user);
 }
