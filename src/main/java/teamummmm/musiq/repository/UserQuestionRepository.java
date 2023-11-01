14dde296cc650335fc6de1738949e827b65d574c
min-jp <mincjiip@gmail.com>
2023-11-02 04:08:11 +0900

    feat: Implemented mainQuestionService in QuestionService. Implemented findByUser_UserId in UserQuestionRepository.

--- src/main/java/teamummmm/musiq/repository/UserQuestionRepository.java
+++ src/main/java/teamummmm/musiq/repository/UserQuestionRepository.java
@@ -2,12 +2,11 @@ package teamummmm.musiq.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import teamummmm.musiq.model.UserProfileEntity;
 import teamummmm.musiq.model.UserQuestionEntity;
 
 import java.util.List;
 
 @Repository
 public interface UserQuestionRepository extends JpaRepository<UserQuestionEntity, Long> {
-    List<UserQuestionEntity> findByUser(UserProfileEntity user);
+    List<UserQuestionEntity> findByUser_UserId(Long userId);
 }
--- src/main/java/teamummmm/musiq/service/QuestionService.java
+++ src/main/java/teamummmm/musiq/service/QuestionService.java
@@ -1,22 +1,27 @@
 package teamummmm.musiq.service;
 
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import teamummmm.musiq.dto.RequestQuestionDTO;
+import teamummmm.musiq.model.UserQuestionEntity;
 import teamummmm.musiq.repository.CommonQuestionRepository;
 import teamummmm.musiq.repository.UserQuestionRepository;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor  // 생성자 주입
+@AllArgsConstructor  // 생성자 주입
 public class QuestionService {
     private UserQuestionRepository userQuestionRepository;
     private CommonQuestionRepository commonQuestionRepository;
 
+    // TODO
+    //  dto 하나만 리턴하도록 변경
     public List<RequestQuestionDTO> mainQuestionService(Long userId, boolean refresh) {
-        // (1) 리프레시 여부 확인
+        // TODO
+        //  리프레시에 따른 로직 구현
+        // 리프레시 여부 확인
         if (refresh) {  // 일반 질문 호출
 
         }
@@ -24,18 +29,32 @@ public class QuestionService {
 
         }
 
-        // (2) 유저 아이디 받아서 질문 리턴 (유저질물)
+        // 유저 아이디 받아서 질문 리턴 (유저질문)
+        List<UserQuestionEntity> entities = userQuestionRepository.findByUser_UserId(userId);
 
+        // 데이터 적재
+        List<RequestQuestionDTO> dtos = entities.stream()  // stream 이용해서 변환
+                .map(entity -> {
+                    RequestQuestionDTO dto = RequestQuestionDTO.builder()
+                            .question_id(entity.getUserQuestionId())  // 오브젝트 아이디 (userQuestionId)
+                            .question_message(entity.getCommonQuestion().getQuestionMsg())  // 질문 내용 (questionMsg)
+                            .emoji(entity.getCommonQuestion().getEmoji())  // 질문 이모지 (emoji)
+                            // TODO
+                            //  main_color 로직 구현
+                            .build();
+                    return dto;
+                })
+                .collect(Collectors.toList());
 
-        // (3) 유저질문에서의 id바탕으로 검색?
-
-        // (4) 데이터 적재
-        // (5) 리턴
-        List<RequestQuestionDTO> list = new ArrayList<>();
-        return list;
+        // 리턴
+        return dtos;
     }
 
+    // TODO
+    //  리프레시 질문 구현
     // 리프레시 질문
 
+    // TODO
+    //  일반 질문 구현
     // 일반 질문
 }
