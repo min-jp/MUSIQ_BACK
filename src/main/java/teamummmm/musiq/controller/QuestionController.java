1849c85b3eacd3d4e1df3f4335f3fea6e0d249a5
min-jp <mincjiip@gmail.com>
2023-11-02 19:30:58 +0900

    feat: Implemented answeredQuestionRequest in QuestionController.

--- src/main/java/teamummmm/musiq/controller/QuestionController.java
+++ src/main/java/teamummmm/musiq/controller/QuestionController.java
@@ -1,6 +1,6 @@
 package teamummmm.musiq.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +13,9 @@ import teamummmm.musiq.service.QuestionService;
 import java.util.List;
 
 @RestController
+@AllArgsConstructor  // 생성자 주입
 @RequestMapping("question")
 public class QuestionController {
-    @Autowired
     private QuestionService service;  // service
 
     @GetMapping("/main")
@@ -28,6 +28,29 @@ public class QuestionController {
             //  유저 아이디
             Long temporaryUserId = 1L;  // 임시 유저 아이디
             RequestQuestionDTO dto = service.mainQuestionService(temporaryUserId, refresh);  // RequestQuestionDTO 생성
+            ResponseDTO<RequestQuestionDTO> response = ResponseDTO.<RequestQuestionDTO>builder()
+                    .data(List.of(dto))
+                    .build();  // UserQuestionDTO 이용하여 ResponseDTO 초기화
+            return ResponseEntity.ok().body(response);  // ResponseDTO 리턴
+        } catch (Exception e) {
+            String error = e.getMessage();
+            ResponseDTO<RequestQuestionDTO> response = ResponseDTO.<RequestQuestionDTO>builder()
+                    .error(error)
+                    .build();
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    @GetMapping("/answered")
+    public ResponseEntity<?> answeredQuestionRequest(
+            @RequestParam(value = "user_id") Long userId,
+            @RequestParam(value = "refresh") boolean refresh
+    ) {
+        try {
+            // TODO
+            //  유저 아이디
+            Long temporaryUserId = 1L;  // 임시 유저 아이디
+            RequestQuestionDTO dto = service.answeredQuestionService(temporaryUserId, refresh);  // RequestQuestionDTO 생성
             ResponseDTO<RequestQuestionDTO> response = ResponseDTO.<RequestQuestionDTO>builder()
                     .data(List.of(dto))
                     .build();  // UserQuestionDTO 이용하여 ResponseDTO 초기화
