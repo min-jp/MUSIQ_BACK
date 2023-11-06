23c7a795b103830eb3b505de2ab255de4e6c38fe
min-jp <mincjiip@gmail.com>
2023-11-07 02:09:55 +0900

    feat: Added music_name, artist_name, and cover_image to SearchResultDTO

--- src/main/java/teamummmm/musiq/dto/SearchResultDTO.java
+++ src/main/java/teamummmm/musiq/dto/SearchResultDTO.java
@@ -11,4 +11,18 @@ import lombok.NoArgsConstructor;
 @Data
 public class SearchResultDTO {
     private String music_id;  // 음악 아이디 (musicId)
+
+    private String music_name;
+
+    private String artist_name;
+
+    private CoverImage cover_image;
+
+    @Builder
+    @Data
+    public static class CoverImage {
+        private String url;
+        private int height;
+        private int width;
+    }
 }
