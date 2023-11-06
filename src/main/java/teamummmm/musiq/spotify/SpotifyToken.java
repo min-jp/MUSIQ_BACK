0e70a4b4b8d2654e703dfd807d76b811eae5bb0a
min-jp <mincjiip@gmail.com>
2023-11-07 02:30:19 +0900

    fix: remove spotify-api-key.json

--- src/main/java/teamummmm/musiq/spotify/SpotifyToken.java
+++ src/main/java/teamummmm/musiq/spotify/SpotifyToken.java
@@ -1,7 +1,5 @@
 package teamummmm.musiq.spotify;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import org.apache.hc.core5.http.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
@@ -11,35 +9,11 @@ import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
 import se.michaelthelin.spotify.model_objects.credentials.ClientCredentials;
 import se.michaelthelin.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 
 @Service
 @PropertySource("classpath:application-secret.properties")
 public class SpotifyToken {
-    /*
-    // json 파일 사용하는 경우
-    private static final FileReader fileReader;  // json 파일 읽어옴
-
-    static {
-        FileReader reader = null;
-        try {
-            reader = new FileReader("src/main/resources/spotify-api-key.json");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        fileReader = reader;
-    }
-
-    private final JsonObject jsonObject = JsonParser.parseReader(fileReader).getAsJsonObject();  //json object로 변환
-
-    private final String clientId = jsonObject.get("client_id").getAsString();
-    private final String clientSecret = jsonObject.get("client_secret").getAsString();
-     */
-
-
-    // 스프링 사용하는 경우
     @Value("${spotify.client.id}")
     private String clientId;
     @Value("${spotify.client.secret}")
