f8709bdf7f2d8c2e65d1823cbd2d3e646067f2c0
min-jp <mincjiip@gmail.com>
2023-11-08 02:22:05 +0900

    feat: Implemented findBestColor in AnswerRepository

--- src/main/java/teamummmm/musiq/repository/AnswerRepository.java
+++ src/main/java/teamummmm/musiq/repository/AnswerRepository.java
@@ -1,9 +1,18 @@
 package teamummmm.musiq.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import teamummmm.musiq.model.AnswerEntity;
+import teamummmm.musiq.model.ColorVal;
 
 @Repository
 public interface AnswerRepository extends JpaRepository<AnswerEntity, Long> {
+    @Query("SELECT a.musicInfo.musicColor " +
+            "FROM AnswerEntity a " +
+            "WHERE a.userQuestion.userQuestionId = ?1 " +
+            "GROUP BY a.musicInfo.musicColor " +
+            "ORDER BY COUNT(a.musicInfo.musicColor) DESC " +
+            "LIMIT 1")
+    ColorVal findBestColor(Long questionId);  // 질문의 메인 컬러 찾기
 }
