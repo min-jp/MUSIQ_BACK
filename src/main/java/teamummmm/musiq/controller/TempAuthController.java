f2480568fbd6f543618621915beaa0c7f98ce07f
min-jp <mincjiip@gmail.com>
2023-11-08 21:26:52 +0900

    feat: Implemented TempAuthService and TempAuthController

---
+++ src/main/java/teamummmm/musiq/controller/TempAuthController.java
@@ -0,0 +1,36 @@
+package teamummmm.musiq.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import teamummmm.musiq.dto.ErrorDTO;
+import teamummmm.musiq.service.TempAuthService;
+
+// FIXME
+//  auth 구현
+@RestController
+@RequiredArgsConstructor  // 생성자 주입
+@RequestMapping("auth")
+public class TempAuthController {
+    private final TempAuthService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(
+            @RequestParam(value = "login_id") String loginId
+    ) {
+        try {
+            service.registerService(loginId);
+
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            ErrorDTO errorDTO = ErrorDTO.builder()
+                    .error(e.getMessage())
+                    .build();
+
+            return ResponseEntity.badRequest().body(errorDTO);
+        }
+    }
+}
---
+++ src/main/java/teamummmm/musiq/service/TempAuthService.java
@@ -0,0 +1,43 @@
+package teamummmm.musiq.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import teamummmm.musiq.model.CommonQuestionEntity;
+import teamummmm.musiq.model.UserProfileEntity;
+import teamummmm.musiq.model.UserQuestionEntity;
+import teamummmm.musiq.repository.CommonQuestionRepository;
+import teamummmm.musiq.repository.UserProfileRepository;
+import teamummmm.musiq.repository.UserQuestionRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor  // 생성자 주입
+public class TempAuthService {
+    private final UserProfileRepository userProfileRepository;
+    private final UserQuestionRepository userQuestionRepository;
+    private final CommonQuestionRepository commonQuestionRepository;
+
+    public void registerService(final String loginId) {
+        if (userProfileRepository.existsByLoginId(loginId)) {  // 로그인 정보 확인
+            throw new RuntimeException("Username already exists");  // 존재하면 오류
+        }
+
+        UserProfileEntity userProfileEntity = UserProfileEntity.builder()
+                .loginId(loginId)
+                .build();  // UserProfileEntity 생성
+
+        UserProfileEntity savedEntity = userProfileRepository.save(userProfileEntity);  // 저장
+
+        // 유저 질문 생성
+        List<CommonQuestionEntity> commonQuestionEntityList = commonQuestionRepository.findAll();
+        commonQuestionEntityList.forEach(commonQuestionEntity -> {
+            UserQuestionEntity userQuestionEntity = UserQuestionEntity.builder()
+                    .user(savedEntity)
+                    .commonQuestion(commonQuestionEntity)
+                    .build();  // UserQuestionEntity 생성
+
+            userQuestionRepository.save(userQuestionEntity);  //저장
+        });
+    }
+}
