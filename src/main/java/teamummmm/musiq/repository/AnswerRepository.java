0d1de68b61447aa2abcb4387b9b62ae1f56c0a8f
min-jp <mincjiip@gmail.com>
2023-11-10 01:29:11 +0900

    feat: Implemented countMusicColorsByUserQuestionId in AnswerRepository

--- src/main/java/teamummmm/musiq/repository/AnswerRepository.java
+++ src/main/java/teamummmm/musiq/repository/AnswerRepository.java
@@ -7,6 +7,7 @@ import teamummmm.musiq.model.AnswerEntity;
 import teamummmm.musiq.model.ColorVal;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface AnswerRepository extends JpaRepository<AnswerEntity, Long> {
@@ -16,11 +17,18 @@ public interface AnswerRepository extends JpaRepository<AnswerEntity, Long> {
             "GROUP BY a.musicInfo.musicColor " +
             "ORDER BY COUNT(a.musicInfo.musicColor) DESC " +
             "LIMIT 1")
-    ColorVal findBestColor(Long questionId);  // 질문의 메인 컬러 찾기
+    ColorVal findBestColor(Long userQuestionId);  // 질문의 메인 컬러 찾기
 
-    @Query("SELECT COUNT(e)  = 1 " +
+    @Query("SELECT COUNT(e) = 1 " +
             "FROM AnswerEntity e " +
             "WHERE e.userQuestion.userQuestionId = ?1 " +
             "AND e.answerDate = ?2")
     boolean existsSingleEntryByUserQuestionAndAnswerDate(Long userQuestionId, LocalDate nowDate);
+
+    @Query("SELECT a.musicInfo.musicColor, COUNT(a.musicInfo.musicColor) AS colorCount " +
+            "FROM AnswerEntity a " +
+            "WHERE a.userQuestion.userQuestionId = ?1 " +
+            "GROUP BY a.musicInfo.musicColor " +
+            "ORDER BY colorCount DESC")
+    List<Object[]> countMusicColorsByUserQuestionId(Long userQuestionId);
 }
