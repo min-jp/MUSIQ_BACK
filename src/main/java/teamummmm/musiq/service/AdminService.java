286a46a104ea3380b714030efc668e266d5204d6
min-jp <mincjiip@gmail.com>
2023-11-12 04:21:00 +0900

    written the initial draft of AdminService and AdminController

---
+++ src/main/java/teamummmm/musiq/service/AdminService.java
@@ -0,0 +1,9 @@
+package teamummmm.musiq.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor  // 생성자 주입
+public class AdminService {
+}
