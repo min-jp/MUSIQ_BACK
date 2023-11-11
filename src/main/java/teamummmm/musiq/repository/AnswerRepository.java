6f991bc2d4cebb94a39aa66c96e7e84d581d2e3f
min-jp <mincjiip@gmail.com>
2023-11-11 22:31:29 +0900

    add: Added comments

--- src/main/java/teamummmm/musiq/repository/AnswerRepository.java
+++ src/main/java/teamummmm/musiq/repository/AnswerRepository.java
@@ -23,15 +23,14 @@ public interface AnswerRepository extends JpaRepository<AnswerEntity, Long> {
             "FROM AnswerEntity e " +
             "WHERE e.userQuestion.userQuestionId = ?1 " +
             "AND e.answerDate = ?2")
-    boolean isFirstAnswer(Long userQuestionId, LocalDate nowDate);
+    boolean isFirstAnswer(Long userQuestionId, LocalDate nowDate);  // 오늘 남긴 첫 번째 질문인지 확인
 
     @Query("SELECT a.musicInfo.musicColor, COUNT(a.musicInfo.musicColor) AS colorCount " +
             "FROM AnswerEntity a " +
             "WHERE a.userQuestion.userQuestionId = ?1 " +
             "GROUP BY a.musicInfo.musicColor " +
             "ORDER BY colorCount DESC")
-    List<Object[]> countMusicColors(Long userQuestionId);
+    List<Object[]> countMusicColors(Long userQuestionId);  // 질문별 색깔 비율
 
-    List<AnswerEntity> findByUserQuestion_UserQuestionId(Long userQuestionId);
-    // TODO 주석 달기
+    List<AnswerEntity> findByUserQuestion_UserQuestionId(Long userQuestionId);  // UserQuestionId로 AnswerEntity 찾기
 }
