f8ac87e18f97d2dfbbff25eae6482465a33b685b
min-jp <mincjiip@gmail.com>
2023-11-12 19:46:40 +0900

    feat: add the initial draft of the CaptionDTO code

---
+++ src/main/java/teamummmm/musiq/dto/CaptionDTO.java
@@ -0,0 +1,14 @@
+package teamummmm.musiq.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CaptionDTO {
+    private String caption_text;  // 캡션 내용
+}
