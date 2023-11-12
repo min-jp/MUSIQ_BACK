0018e6161272ebeeb9389b78cd08fc485cc6ba2e
min-jp <mincjiip@gmail.com>
2023-11-12 19:18:37 +0900

    feat: Implemented playSongController in PlaySongController

--- src/main/java/teamummmm/musiq/controller/PlaySongController.java
+++ src/main/java/teamummmm/musiq/controller/PlaySongController.java
@@ -2,11 +2,9 @@ package teamummmm.musiq.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import teamummmm.musiq.dto.ErrorDTO;
+import teamummmm.musiq.dto.PlaySongInfoDTO;
 import teamummmm.musiq.service.PlaySongService;
 
 @RestController
@@ -14,6 +12,25 @@ import teamummmm.musiq.service.PlaySongService;
 @RequestMapping("play-song")
 public class PlaySongController {
     private final PlaySongService service;
+
+    @GetMapping("/play")
+    private ResponseEntity<?> playSongController(
+            @RequestParam(value = "answer_id") Long answerId
+    ) {
+        try {
+            PlaySongInfoDTO dto = service.playSongService(answerId);  // PlaySongInfoDTO 생성
+
+            return ResponseEntity.ok().body(dto);  // PlaySongInfoDTO 리턴
+        } catch (Exception e) {
+            ErrorDTO errorDTO = ErrorDTO.builder()
+                    .error(e.getMessage())
+                    .build();
+
+            System.out.println("\nError: " + e.getMessage());
+
+            return ResponseEntity.badRequest().body(errorDTO);
+        }
+    }
 
     @PostMapping("/caption")
     public ResponseEntity<?> captionController(
