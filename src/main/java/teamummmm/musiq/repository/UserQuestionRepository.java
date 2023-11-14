80006d59395e254aa7243aba1bb47a6e1f99bc5c
min-jp <mincjiip@gmail.com>
2023-11-14 23:26:21 +0900

    feat: Modified findByUser_UserIdAndAnswerPageAnsListIsEmpty method to mainQuestionList and defaultQuestionList

--- src/main/java/teamummmm/musiq/repository/UserQuestionRepository.java
+++ src/main/java/teamummmm/musiq/repository/UserQuestionRepository.java
@@ -1,6 +1,7 @@
 package teamummmm.musiq.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import teamummmm.musiq.model.UserQuestionEntity;
 
@@ -10,5 +11,17 @@ import java.util.List;
 public interface UserQuestionRepository extends JpaRepository<UserQuestionEntity, Long> {
     List<UserQuestionEntity> findByUser_UserIdAndAnswerPageAnsListIsNotEmpty(Long userId);  // 답변한 질문 리스트
 
-    List<UserQuestionEntity> findByUser_UserIdAndAnswerPageAnsListIsEmpty(Long userId);  // 답변하지 않은 질문 리스트
+    @Query("SELECT u " +
+            "FROM UserQuestionEntity u " +
+            "WHERE u.user.userId = ?1 " +
+            "AND u.answerPageAnsList IS EMPTY " +
+            "AND u.commonQuestion.category != 0")  // 디폴트 질문 제외
+    List<UserQuestionEntity> mainQuestionList(Long userId);  // 디폴트 질문 제외한 메인 질문
+
+    @Query("SELECT u " +
+            "FROM UserQuestionEntity u " +
+            "WHERE u.user.userId = ?1 " +
+            "AND u.answerPageAnsList IS EMPTY " +
+            "AND u.commonQuestion.category = 0")
+    List<UserQuestionEntity> defaultQuestionList(Long userId);  // 디폴트 질문
 }
