df4230fe333cfb0ffe914e0ba282759db143bd14
min-jp <mincjiip@gmail.com>
2023-11-14 23:29:25 +0900

    feat: Renamed the existing isFirstAnswer method to isFirstAnswerToday and added a new isFirstAnswer method

--- src/main/java/teamummmm/musiq/repository/AnswerRepository.java
+++ src/main/java/teamummmm/musiq/repository/AnswerRepository.java
@@ -11,6 +11,8 @@ import java.util.List;
 
 @Repository
 public interface AnswerRepository extends JpaRepository<AnswerEntity, Long> {
+    // TODO
+    //  가장 큰 것이 여러개일 때 가장 최근에 답변한 색으로 변경
     @Query("SELECT a.musicInfo.musicColor " +
             "FROM AnswerEntity a " +
             "WHERE a.userQuestion.userQuestionId = ?1 " +
@@ -23,7 +25,12 @@ public interface AnswerRepository extends JpaRepository<AnswerEntity, Long> {
             "FROM AnswerEntity e " +
             "WHERE e.userQuestion.userQuestionId = ?1 " +
             "AND e.answerDate = ?2")
-    boolean isFirstAnswer(Long userQuestionId, LocalDate nowDate);  // 오늘 남긴 첫 번째 질문인지 확인
+    Boolean isFirstAnswerToday(Long userQuestionId, LocalDate nowDate);  // 오늘 남긴 첫 번째 질문인지 확인
+
+    @Query("SELECT COUNT(e) = 1 " +
+            "FROM AnswerEntity e " +
+            "WHERE e.userQuestion.userQuestionId = ?1")
+    Boolean isFirstAnswer(Long userQuestionId);  // 질문에 대한 첫 번째 질문인지 확인
 
     @Query("SELECT a.musicInfo.musicColor, COUNT(a.musicInfo.musicColor) AS colorCount " +
             "FROM AnswerEntity a " +
@@ -37,4 +44,6 @@ public interface AnswerRepository extends JpaRepository<AnswerEntity, Long> {
     List<AnswerEntity> findByUserQuestion_User_UserId(Long userId);  // UserId로 AnswerEntity 찾기
 
     List<AnswerEntity> findByAnswerDateAndUserQuestion_User_UserId(LocalDate answerDate, Long userId);  // AnswerDate와 UserId로 AnswerEntity 찾기
+
+    Boolean existsByUserQuestion_User_UserId(Long userId);  // 대답을 한 적이 있는지 확인
 }
