80f0490eac6d52f2b15daab3e961c96771eabd24
min-jp <mincjiip@gmail.com>
2023-11-14 23:25:12 +0900

    fix: Modified to convert boolean values to Boolean

--- src/main/java/teamummmm/musiq/repository/UserProfileRepository.java
+++ src/main/java/teamummmm/musiq/repository/UserProfileRepository.java
@@ -6,7 +6,7 @@ import teamummmm.musiq.model.UserProfileEntity;
 
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfileEntity, Long> {
-    boolean existsByLoginId(String loginId);  // login id가 이미 존재하는지 확인
+    Boolean existsByLoginId(String loginId);  // login id가 이미 존재하는지 확인
 
     UserProfileEntity findByLoginId(String loginId);  // login id로 UserProfileEntity 찾기
 }
