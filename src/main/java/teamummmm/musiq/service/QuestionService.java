83f7ad1a0a373ff9ef2dfd51d8da8d3a67d9431f
min-jp <mincjiip@gmail.com>
2023-11-14 23:33:18 +0900

    feat: Implemented findBestMainQuestion in mainQuestionService. Implemented mainQuestionService logic.

--- src/main/java/teamummmm/musiq/service/QuestionService.java
+++ src/main/java/teamummmm/musiq/service/QuestionService.java
@@ -3,65 +3,46 @@ package teamummmm.musiq.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import teamummmm.musiq.dto.RequestQuestionDTO;
-import teamummmm.musiq.model.ColorVal;
-import teamummmm.musiq.model.UserQuestionEntity;
+import teamummmm.musiq.model.*;
 import teamummmm.musiq.repository.AnswerRepository;
+import teamummmm.musiq.repository.UserProfileRepository;
 import teamummmm.musiq.repository.UserQuestionRepository;
 
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor  // 생성자 주입
 public class QuestionService {
     private final UserQuestionRepository userQuestionRepository;
     private final AnswerRepository answerRepository;
+    private final UserProfileRepository userProfileRepository;
 
     public RequestQuestionDTO mainQuestionService(final Long userId, final boolean refresh) {
-        // 유저 아이디 받아서 질문 리턴 (유저질문)
-        List<UserQuestionEntity> entities = userQuestionRepository.findByUser_UserIdAndAnswerPageAnsListIsEmpty(userId);
+        UserQuestionEntity entity;  // 엔티티 선언
 
-        // 현재 유저의 대답 중 가장 가장 맞는 대답 찾기
-
-        // 그 중에 가장 콜이 적은 대답 찾기
-
-        // TODO
-        //  리프레시에 따른 로직 구현
-        // 리프레시 여부 확인
-        if (refresh) {  // 새로고침한 질문 호출
-
+        if (!answerRepository.existsByUserQuestion_User_UserId(userId)) {  // 처음 호출하는 경우
+            entity = userQuestionRepository.defaultQuestionList(userId).get(0);  // 디폴트 질문 호출
         }
-        else {  // 일반 질문 호출
+        else {  // 답한 질문이 있는 경우
+            if (refresh) {  // 새로고침한 경우
+                entity = findBestMainQuestion(userId);  // 기존 best 메인 질문
+                entity.updateRankingCount(entity.getRankingCount() + 1);  // 랭킹 카운트 증가
+                userQuestionRepository.save(entity); // 저장 후 다시 best 메인 질문 탐색
+            }
 
+            entity = findBestMainQuestion(userId);  // 사용자 값과 가장 가까운 메인 질문 찾기
+
+            // TODO
+            //  후속질문 출력하도록 로직 수정
         }
 
-        // TODO
-        //  선택 코드 위 로직 안에 넣기
-        // 엔티티 중 선택
-        Random random = new Random();
-        int randomNum = random.nextInt(entities.size());
-        UserQuestionEntity entity = entities.get(randomNum);
-
-        // 콜 수 업데이트
-
-        // 데이터 적재 코드
-
-        // 리턴
         return RequestQuestionDTO.builder()
                 .question_id(entity.getUserQuestionId())  // 오브젝트 아이디 (userQuestionId)
                 .question_message(entity.getCommonQuestion().getQuestionMsg())  // 질문 내용 (questionMsg)
                 .main_color(ColorVal.DEFAULT.ordinal())
                 .emoji(entity.getCommonQuestion().getEmoji())  // 질문 이모지 (emoji)
-                .build();
+                .build();  // RequestQuestionDTO 리턴
     }
-
-    // TODO
-    //  메인 질문 리프레시 질문 구현
-    // 메인 리프레시 질문
-
-    // TODO
-    //  메인 질문 일반 질문 구현
-    // 메인 일반 질문
 
     public RequestQuestionDTO answeredQuestionService(final Long userId, final boolean refresh) {
         // 유저 아이디 받아서 질문 리턴 (유저질문)
@@ -73,24 +54,11 @@ public class QuestionService {
 
         // TODO
         //  리프레시에 따른 로직 구현
-        // 리프레시 여부 확인
-        if (refresh) {  // 새로고침한 질문 호출
 
-        }
-        else {  // 일반 질문 호출
-
-        }
-
-        // TODO
-        //  선택 코드 위 로직 안에 넣기
         // 엔티티 중 선택
         Random random = new Random();
         int randomNum = random.nextInt(entities.size());
-        UserQuestionEntity entity = entities.get(randomNum);
-
-        // 콜 수 업데이트
-
-        // 데이터 적재 코드
+        UserQuestionEntity entity = entities.get(randomNum);  // 랜덤 선택
 
         // 리턴
         return RequestQuestionDTO.builder()
@@ -101,11 +69,30 @@ public class QuestionService {
                 .build();
     }
 
-    // TODO
-    //  대답한 질문 리프레시 질문 구현
-    // 대답한 리프레시 질문
+    // 사용자 값과 가장 가까운 메인 질문 찾기
+    private UserQuestionEntity findBestMainQuestion(final Long userId) {
+        List<UserQuestionEntity> entities = userQuestionRepository.mainQuestionList(userId);  // 대답 안한 UserQuestionEntity 찾기
 
-    // TODO
-    //  대답한 질문 일반 질문 구현
-    // 대답한 일반 질문
+        if (entities.isEmpty()) {  // 메인 질문이 없는 경우
+            return UserQuestionEntity.builder().build();  // 빈 DTO 리턴
+        }
+
+        // 유저의 파라미터 찾기
+        UserProfileEntity userProfileEntity = userProfileRepository.findById(userId).get();  // user 엔티티 찾기
+        final Float userDanceability = userProfileEntity.getDanceability();  // 유저의 danceability
+        final Float userEnergy = userProfileEntity.getEnergy();  // 유저의 energy
+        final Float userValence = userProfileEntity.getValence();  // 유저의 valence
+
+        // 유클리드 거리 계산해서 거리가 가장 가까운 질문 찾기
+        return entities.stream().min(Comparator.comparingDouble(entity -> {  // 가장 작은 값 찾기
+            CommonQuestionEntity commonQuestion = entity.getCommonQuestion();  // feature를 위한 CommonQuestionEntity
+
+            return (Math.log10(entity.getRankingCount() + 1)/Math.log10(3) +  // 랭킹 카운트에 따라 log3(x+1)의 크기로 거리에 더함
+                    Math.sqrt(  // 루트
+                            Math.pow(userDanceability - commonQuestion.getDanceability(), 2) +  // danceability 값의 차의 제곱
+                            Math.pow(userEnergy - commonQuestion.getEnergy(), 2) +  // energy 값의 차의 제곱
+                            Math.pow(userValence - commonQuestion.getValence(), 2)  // valence 값의 차의 제곱
+                    ));  // 유클리드 거리 계산
+        })).orElse(null);  // 가장 가까운 UserQuestionEntity 찾은 뒤 리턴
+    }
 }
